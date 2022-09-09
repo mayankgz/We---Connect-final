70c0f1a8bfcc77a74c8914abef5568f0a8f2182f
mayankgz <104299023+mayankgz@users.noreply.github.com>
2022-09-09 10:47:05 +0530

    Update connection.js

--- BACKEND/db/connection.js
+++ BACKEND/db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URL ='mongodb+srv://Dhruv:dhruv8287356826@cluster0.06wn87f.mongodb.net/?retryWrites=true&w=majority';
+const URL ='mongodb+srv://mayank:mayankgagz123@cluster0.vuzsy2h.mongodb.net/We-Connect?retryWrites=true&w=majority';
 mongoose.connect(URL,{maxPoolSize:5},(err)=>{
     if(err){
         console.log('DB Error ', err);
